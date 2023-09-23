7e0049293847fb2419515b971797a86feffdf50a
bedirhan4129 <bedirhanbayraktar4129@gmail.com>
2023-09-23 05:56:48 +0300

    tasarım ve digerleri degistirildi.

--- enginkirtasiye/settings.py
+++ enginkirtasiye/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'enginkirtasiye.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['core/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
